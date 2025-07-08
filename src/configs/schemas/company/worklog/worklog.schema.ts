e3a75b44a3734222686fce96d0d8880fd69d0b3e
Aashutosh Parajuli <aparajuli1@hamropatro.com>
2025-07-08 14:58:32 +0545

    fix: remove unused IAdminForm type from worklog schema

--- src/configs/schemas/company/worklog/worklog.schema.ts
+++ src/configs/schemas/company/worklog/worklog.schema.ts
@@ -4,5 +4,3 @@ export const worklogSchema = Yup.object().shape({
     taskCompleted: Yup.string().required('Task completed is required'),
     taskPlanned: Yup.string().required('Task planned is required'),
 })
-
-export type IAdminForm = Yup.InferType<typeof adminSchema>;
