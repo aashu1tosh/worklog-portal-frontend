76d2e69eb27dfdf055a0fc6d7c1d36690d1197e4
Aashutosh Parajuli <aashutosh282@gmail.com>
2025-07-15 22:21:12 +0545

    feat: remove unused watch function from useForm in Profile component

--- src/pages/dashboard/profile/Profile.tsx
+++ src/pages/dashboard/profile/Profile.tsx
@@ -31,7 +31,6 @@ const Profile = () => {
     const {
         register,
         handleSubmit,
-        watch,
         reset,
         formState: { isSubmitting, errors },
     } = useForm<IUpdateProfile>({
